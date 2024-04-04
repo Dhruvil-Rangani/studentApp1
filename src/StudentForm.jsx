93ad1874161e8dd1cd4e057524f56ba90cd75c7a
Dhruvil Rangani <drangani@my.centennialcollege.ca>
2024-04-03 23:55:02 -0400

    Update StudentForm.jsx

--- src/StudentForm.jsx
+++ src/StudentForm.jsx
@@ -18,6 +18,7 @@ const StudentForm = ({ onSubmit }) => {
       <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
       <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
       <input type="tel" placeholder="Phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
+      <button type="submit">Add student</button>
       <button type="submit">Submit</button>
     </form>
   );
